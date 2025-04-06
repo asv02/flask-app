223e6ff3ee3306fe057ef6bdb3a4ebbc460a6a87
asv02 <200106025@hbtu.ac.in>
2025-04-06 12:29:52 +0530

    jinga2 tut

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, render_template
-
 app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    value1 = 20
+    value2 = "Template value2"
+    return render_template('home.html',value1=value1,value2=value2)
 
 @app.route('/api/auth/login')
 def login():
     greet = request.args.get('greet')
     return str(dir(request))
 
+@app.template_filter('reverse_string')
+def reverseString(s):
+    return s[::-1]
 
 if __name__ == '__main__':
     app.run(debug=True,host='127.0.0.1',port=5500)
